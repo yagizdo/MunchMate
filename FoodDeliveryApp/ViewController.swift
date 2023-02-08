1ad7e4a54f783be0bcf6c804ee4ba56082c5974b
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-02-08 16:33:22 +0300

    Initial Commit

---
+++ FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Yılmaz Yağız Dokumacı on 8.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
