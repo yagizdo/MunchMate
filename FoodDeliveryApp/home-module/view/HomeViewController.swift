092ba33e895446376aa9c193498fb64271d0ff87
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-02-16 19:25:08 +0300

    Added navbar in home view

--- FoodDeliveryApp/home-module/view/HomeViewController.swift
+++ FoodDeliveryApp/home-module/view/HomeViewController.swift
@@ -9,15 +9,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var navbarProfileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         /// Hide Navigation Controller
         self.navigationController?.isNavigationBarHidden = true
         // disable pop gesture
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
-    }
-
-    @IBAction func logoutTestButton(_ sender: Any) {
-        AuthService.shared.logout()
+        
+        // We turn the profile picture into a circle.
+        navbarProfileImage.layer.borderWidth = 1
+        navbarProfileImage.layer.masksToBounds = false
+        navbarProfileImage.layer.borderColor = UIColor.black.cgColor
+        navbarProfileImage.layer.cornerRadius = navbarProfileImage.frame.height/2
+        navbarProfileImage.clipsToBounds = true
     }
 }
