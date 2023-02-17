d6181fecce69fc59072754aa83005139ff1f96a7
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-02-17 18:15:40 +0300

    added home slider collection view

---
+++ FoodDeliveryApp/home-module/collectionview/HomeSliderCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  homeSliderCollectionViewCell.swift
+//  FoodDeliveryApp
+//
+//  Created by Yılmaz Yağız Dokumacı on 17.02.2023.
+//
+
+import UIKit
+
+class HomeSliderCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var background: UIView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func orderNowButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var vectorImage: UIImageView!
+    
+    
+}
--- FoodDeliveryApp/home-module/view/HomeViewController.swift
+++ FoodDeliveryApp/home-module/view/HomeViewController.swift
@@ -11,6 +11,8 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var navbarProfileImage: UIImageView!
     
+    @IBOutlet weak var homeSliderCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         /// Hide Navigation Controller
@@ -24,5 +26,34 @@ class HomeViewController: UIViewController {
         navbarProfileImage.layer.borderColor = UIColor.black.cgColor
         navbarProfileImage.layer.cornerRadius = navbarProfileImage.frame.height/2
         navbarProfileImage.clipsToBounds = true
+        
+        // Set homeSlider Collection View
+        homeSliderCollectionView.delegate = self
+        homeSliderCollectionView.dataSource = self
     }
+}
+
+extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = homeSliderCollectionView.dequeueReusableCell(withReuseIdentifier: "homeSliderCell", for: indexPath) as! HomeSliderCollectionViewCell
+        
+        cell.background.backgroundColor = UIColor(named: "containerBackgroundColor")
+        cell.titleLabel.text = "The Fastest In Delivery Food"
+        cell.titleLabel.spacing = 7
+        cell.vectorImage.image = UIImage(named: "deliverVec")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
